b3272d5325885242bf14dd4348b2bee1ea154ef4
Lisiecki Michał <lisiecki.m@gmail.com>
2018-02-07 21:46:56 +0100

    My solution

--- src/main/scala/com/sparkTutorial/advanced/accumulator/StackOverFlowSurvey.scala
+++ src/main/scala/com/sparkTutorial/advanced/accumulator/StackOverFlowSurvey.scala
@@ -12,6 +12,7 @@ object StackOverFlowSurvey {
     val sparkContext = new SparkContext(conf)
 
     val total = sparkContext.longAccumulator
+    val bytes = sparkContext.longAccumulator
     val missingSalaryMidPoint = sparkContext.longAccumulator
 
     val responseRDD = sparkContext.textFile("in/2016-stack-overflow-survey-responses.csv")
@@ -19,6 +20,7 @@ object StackOverFlowSurvey {
     val responseFromCanada = responseRDD.filter(response => {
       val splits = response.split(Utils.COMMA_DELIMITER, -1)
       total.add(1)
+      bytes.add(response.getBytes().length)
 
       if (splits(14).isEmpty) {
         missingSalaryMidPoint.add(1)
@@ -29,6 +31,7 @@ object StackOverFlowSurvey {
 
     println("Count of responses from Canada: " + responseFromCanada.count())
     println("Total count of responses: " + total.value)
+    println("Size of responses: " + bytes.value)
     println("Count of responses missing salary middle point: " + missingSalaryMidPoint.value)
   }
 }
